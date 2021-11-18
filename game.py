a24cd24b729bf72116c4d2af70881f06e4e649c4
Leticia <leticia.piucco@gmail.com>
2021-11-18 17:25:27 +0000

    Change background, add ghost class

--- game.py
+++ game.py
@@ -14,10 +14,13 @@ class Game:
         pygame.display.set_caption('Ghosts')
         self.load_background()
         self.load_icon()
+        self.walls = ()
+        self.tiles = []
+        self.ghosts = ()
     
     def load_background(self):
-        self.background = pygame.image.load('layout.jpg')
-        self.background = pygame.transform.scale(self.background, (SCREEN_WIDTH, SCREEN_HEIGHT))
+        #self.background = pygame.image.load('layout.jpg')
+        #self.background = pygame.transform.scale(self.background, (SCREEN_WIDTH, SCREEN_HEIGHT))
         self.graph()
     
     def load_icon(self):
@@ -48,8 +51,6 @@ class Game:
 
     def draw(self):
         pygame.display.update()
-        self.screen.blit(self.background, (0,0))
-
     
     def graph(self):
         self.cell_width = SCREEN_WIDTH//COLS
@@ -58,21 +59,79 @@ class Game:
             for y, line in enumerate(file):
                 for x, char in enumerate(line):
                     if char == "%":
-                        pass
-                        pygame.draw.rect(self.background, DARK_BLUE, (x*self.cell_width, y*self.cell_height,
+                        pygame.draw.rect(self.screen, DARK_BLUE, (x*self.cell_width, y*self.cell_height,
                                                                  self.cell_width, self.cell_height))
-                        #self.add_wall(vec(x,y))
+                        self.walls.append(vec(x, y))
                     elif char == "*":
-                        colour = random.choice([GREY, BLACK])
-                        #self.add_tile(vec(x,y))
-                        pygame.draw.rect(self.background, colour, (x*self.cell_width, y*self.cell_height,
+                        colour = random.choice([WHITE, GREY])
+                        pygame.draw.rect(self.screen, colour, (x*self.cell_width, y*self.cell_height,
                                                                   self.cell_width, self.cell_height))
-                
-        """
-        for x in range(SCREEN_WIDTH//self.cell_width):
-            pygame.draw.line(self.background, GREY, (x*self.cell_width, 0),
-                             (x*self.cell_width, SCREEN_HEIGHT))
-        for x in range(SCREEN_HEIGHT//self.cell_height):
-            pygame.draw.line(self.background, GREY, (0, x*self.cell_height),
-                             (SCREEN_WIDTH, x*self.cell_height))
-        """
+                        self.tiles[vec(x, y)] = colour
+                    elif char == "G":
+                        self.add_ghosts()
+
+    def add_ghosts(self):
+        pass
+
+class GhostAgent():
+    def __init__(self, index):
+        self.index = index
+    def get_position(self):
+        pass
+    def get_legal_action(self):
+        pass
+    def get_direction(self):
+        pass
+
+class Directions:
+    NORTH = 'North'
+    SOUTH = 'South'
+    EAST = 'East'
+    WEST = 'West'
+    STOP = 'Stop'
+
+    LEFT = {NORTH: WEST,
+            SOUTH: EAST,
+            EAST:  NORTH,
+            WEST:  SOUTH,
+            STOP:  STOP}
+
+    RIGHT = dict([(y,x) for x, y in LEFT.items()])
+
+    REVERSE = {NORTH: SOUTH,
+               SOUTH: NORTH,
+               EAST: WEST,
+               WEST: EAST,
+               STOP: STOP}
+class Actions:
+    """
+    A collection of static methods for manipulating move actions.
+    """
+    # Directions
+    directions = {Directions.NORTH: (0, 1),
+                   Directions.SOUTH: (0, -1),
+                   Directions.EAST:  (1, 0),
+                   Directions.WEST:  (-1, 0),
+                   Directions.STOP:  (0, 0)}
+
+    directionsAsList = directions.items()
+
+    TOLERANCE = .001
+
+    def get_possible_actions(ghost, walls):
+        possible = []
+        x, y = ghost.get_position()
+        x_int, y_int = int(x + 0.5), int(y + 0.5)
+
+        # In between grid points, all agents must continue straight
+        if (abs(x - x_int) + abs(y - y_int)  > Actions.TOLERANCE):
+            return [ghost.get_direction()]
+
+        for dir, vec in Actions._directionsAsList:
+            dx, dy = vec
+            next_y = y_int + dy
+            next_x = x_int + dx
+            if not walls[next_x][next_y]: possible.append(dir)
+
+        return possible
+    getPossibleActions = staticmethod(get_possible_actions)
